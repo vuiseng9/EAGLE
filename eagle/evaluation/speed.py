75ea02ac1e4bdb28d6b730294f2b9136020c789e
Vui Seng Chua <vui.seng.chua@intel.com>
2024-06-14 12:50:37 -0700

    Enable equivalency check on AR in speed.py and dump mismatch generation to json

--- eagle/evaluation/speed.py
+++ eagle/evaluation/speed.py
@@ -2,11 +2,60 @@ import json
 import argparse
 from transformers import AutoTokenizer
 import numpy as np
+from collections import OrderedDict
+from datetime import date, datetime
+
+def check_output_equivalence(eagle_json, autoregressive_json): 
+    def get_question_response(json_file):
+        qa_dict = {}
+        with open(json_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                json_obj = json.loads(line)
+                qa_dict[json_obj['question_id']] = json_obj['choices'][0]['turns'] # list of turn response
+        return qa_dict
+    
+    ref_dict = get_question_response(autoregressive_json)
+    sd_dict = get_question_response(eagle_json)
+
+    non_intersecting_keys = set(sd_dict.keys()).symmetric_difference(set(ref_dict.keys()))
+
+    if len(non_intersecting_keys) > 0:
+        raise ValueError("Non-equivalent set of input questions")
+    
+    count_match = 0
+    count_mismatch = 0
+    mismatch_dict = []
+    for qid, turns in sd_dict.items():
+        for tid, resp in enumerate(turns):
+            if ref_dict[qid][tid] == resp:
+                count_match += 1
+            else:
+                count_mismatch += 1
+                mismatch_dict.append(OrderedDict({
+                    "question_id": qid,
+                    "turn_id": tid,
+                    "ar_resp": ref_dict[qid][tid],
+                    "sd_resp": resp
+                    }))
+    ts = datetime.now().strftime('%y-%m-%d_%H-%M-%S')
+
+    mismatch_dict.insert(0, {"ar_path": autoregressive_json, "sd_path": eagle_json, "match": count_match, "mismatch": count_mismatch})
+
+    equivalence_rpt = f"{ts}_equivalence_check_match_{count_match},mismatch_{count_mismatch}.json"
+
+    with open(equivalence_rpt, "w") as f:
+        json.dump(mismatch_dict, f, indent=4)
+
+    print(f"Path to mismatch rpt: {equivalence_rpt}")
+    print("")
 
 def main(tokenizer_id, eagle_json, autoregressive_json):
     tokenizer=AutoTokenizer.from_pretrained(tokenizer_id)
     jsonl_file = eagle_json
     jsonl_file_base = autoregressive_json
+
+    check_output_equivalence(jsonl_file, jsonl_file_base)
+
     data = []
     with open(jsonl_file, 'r', encoding='utf-8') as file:
         for line in file:
